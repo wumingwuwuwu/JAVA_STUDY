0a01cec1368c4aaf93fab8251d8fa9454bdcf330
wumingwuwuwu <1743824272@qq.com>
2025-05-11 22:38:25 +0800

    github提交测试

--- src/wofuhuola/jichu/text008.java
+++ src/wofuhuola/jichu/text008.java
@@ -3,5 +3,6 @@ package wofuhuola.jichu;
 public class text008 {
     public static void main(String[] args) {
         System.out.println("github 测试");
+        System.out.println("Hello Github");
     }
 }
